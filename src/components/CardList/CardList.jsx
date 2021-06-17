7986db10133f9ce6ec4b6e2c10a759e639e73d30
Kirill Hrynko <ki.hrynko@gmail.com>
2021-06-17 15:49:47 +0300

    Filter the monsters

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -6,7 +6,7 @@ const CardList = ({monsters}) => {
     return (
         <div className={styles.CardList}>
             {monsters.map(({id, name, email}) => {
-                return <Card id={id} name={name} email={email}/>
+                return <Card key={id} id={id} name={name} email={email}/>
             })}
         </div>
     );
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,6 +4,7 @@ import CardList from "../../components/CardList/CardList";
 class App extends Component {
     state = {
         monsters: [],
+        searchField: '',
     };
 
     componentDidMount() {
@@ -12,11 +13,22 @@ class App extends Component {
             .then(data => this.setState({monsters: data}));
     }
 
-
     render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(({name}) => {
+            return name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        
         return (
             <>
-                <CardList monsters={this.state.monsters}/>
+                <input
+                    type="search"
+                    placeholder="search monsters"
+                    onChange={(e) => this.setState({
+                        searchField: e.target.value
+                    })}
+                />
+                <CardList monsters={filteredMonsters}/>
             </>
         )
     }
